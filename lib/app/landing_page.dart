d4a6826e733bd70e61caa8c5c05469b2a6428544
aospiridonov <aospiridonov@gmail.com>
2022-05-25 15:00:57 +0300

    update gradle add git attrubutes

---
+++ lib/app/landing_page.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hourman/app/sign_in/sign_in_page.dart';
+
+import 'home_page.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  User? _user;
+
+  void _updateUser(User? user) {
+    setState(() {
+      _user = user;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_user == null) {
+      return SignInPage(
+        onSignIn: _updateUser,
+      );
+    }
+    return const HomePage();
+  }
+}
